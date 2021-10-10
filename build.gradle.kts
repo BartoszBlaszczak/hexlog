a56035004fdaca5c32a99bdf87ae7075a4822523
Bartosz Błaszczak <bartosz.piotr.blaszczak@gmail.com>
2021-10-10 18:38:04 +0200

    1.2.1 update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
-    id("io.gitlab.arturbosch.detekt").version("1.17.1")
+    kotlin("jvm") version "1.5.31"
+    id("io.gitlab.arturbosch.detekt").version("1.18.1")
     jacoco
 }
 
 group = "hex"
-version ="1.2.0"
+version ="1.2.1"
 
 repositories {
     mavenCentral()
@@ -13,18 +13,18 @@ repositories {
 }
 
 dependencies {
-    val ktorVersion = "1.6.0"
+    val ktorVersion = "1.6.4"
     
-    implementation("org.xerial", "sqlite-jdbc", "3.34.0")
-    implementation("org.slf4j:slf4j-jdk14:1.7.31")
+    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
+    implementation("org.slf4j:slf4j-jdk14:1.7.32")
     implementation("com.github.BartoszBlaszczak:PropertiesLoader:1.0.4")
-    implementation("com.rometools:rome:1.15.0")
+    implementation("com.rometools:rome:1.16.0")
     
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
 
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.3")
     testImplementation("io.ktor:ktor-client-core:$ktorVersion")
     testImplementation("io.ktor:ktor-client-java:$ktorVersion")
 }
